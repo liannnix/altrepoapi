4b7273670ffc481f4d3a4d532a5ad56c84d72bf3
Danil Shein <dshein@altlinux.org>
2021-07-19 12:58:25 +0300

    add site API namespace

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from .task.task import ns as task_ns
 from .package.package import ns as package_ns
 from .packageset.packageset import ns as packageset_ns
+from .site.site import ns as site_ns
 
 api = Api(
     version='1.0',
@@ -15,3 +16,4 @@ api = Api(
 api.add_namespace(task_ns)
 api.add_namespace(package_ns)
 api.add_namespace(packageset_ns)
+api.add_namespace(site_ns)
