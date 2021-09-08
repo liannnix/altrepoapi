3dd0507810bd049971acdc0c3ca982fdd350c376
Dmitry <fruktime@altlinux.org>
2021-09-08 11:20:24 +0300

    add post and get repository status

--- api/packageset/serializers.py
+++ api/packageset/serializers.py
@@ -63,3 +63,43 @@ pkgset_packages_model = ns.model(
         ),
     },
 )
+
+pkgset_status_post_el_model = ns.model(
+    "PackageSetPostElementModel",
+    {
+        "pkgset_name": fields.String(description="package set name"),
+        "rs_start_date": fields.DateTime(description="support start date"),
+        "rs_end_date": fields.DateTime(description="support end date"),
+        "rs_show": fields.Integer(description="0 - hide branch, 1 - show branch"),
+        "rs_description_ru": fields.String(description="html description in Russian in Base64 format"),
+        "rs_description_en": fields.String(description="html description in English in Base64 format")
+    }
+)
+pkgset_status_post_model = ns.model(
+    "PackageSetPostModel",
+    {
+        "branches": fields.Nested(
+            pkgset_status_post_el_model, description="package set info", as_list=True
+        )
+    },
+)
+
+pkgset_status_get_el_model = ns.model(
+    "PackageSetGetElementModel",
+    {
+        "branch": fields.String(description="package set name"),
+        "start_date": fields.DateTime(description="support start date"),
+        "end_date": fields.DateTime(description="support end date"),
+        "show": fields.Integer(description="0 - hide branch, 1 - show branch"),
+        "description_ru": fields.String(description="html description in Russian"),
+        "description_en": fields.String(description="html description in English")
+    }
+)
+pkgset_status_get_model = ns.model(
+    "PackageSetGetModel",
+    {
+        "branches": fields.Nested(
+            pkgset_status_get_el_model, description="package set info", as_list=True
+        )
+    },
+)
