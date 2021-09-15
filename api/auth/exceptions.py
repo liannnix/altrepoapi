589ee6612c6880821fbc40592bcf416d4d5a463e
Danil Shein <dshein@altlinux.org>
2021-09-15 17:51:56 +0300

    auth: exceptions code clean up

--- api/auth/exceptions.py
+++ api/auth/exceptions.py
@@ -11,9 +11,6 @@ class ApiUnauthorized(Unauthorized):
         error_description=None,
     ):
         self.description = description
-        # self.www_auth_value = self.__get_www_auth_value(
-        #     admin_only, error, error_description
-        # )
         self.admin_only = admin_only
         self.error = error
         self.error_description = error_description
@@ -21,31 +18,8 @@ class ApiUnauthorized(Unauthorized):
             self, description=description, response=None, www_authenticate=None
         )
 
-    # def get_headers(self, environ):
-    #     return [("Content-Type", "text/html"), ("WWW-Authenticate", self.www_auth_value)]
-
-    # def __get_www_auth_value(self, admin_only, error, error_description):
-    #     realm = _REALM_ADMIN_USERS if admin_only else _REALM_REGULAR_USERS
-    #     www_auth_value = f'Bearer realm="{realm}"'
-    #     if error:
-    #         www_auth_value += f', error="{error}"'
-    #     if error_description:
-    #         www_auth_value += f', error_description="{error_description}"'
-    #     return www_auth_value
-
 
 class ApiForbidden(Forbidden):
     """Raise status code 403 with WWW-Authenticate header."""
 
     description = "You are not an administrator"
-
-    # def get_headers(self, environ):
-    #     return [
-    #         ("Content-Type", "text/html"),
-    #         (
-    #             "WWW-Authenticate",
-    #             f'Bearer realm="{_REALM_ADMIN_USERS}", '
-    #             'error="insufficient_scope", '
-    #             'error_description="You are not an administrator"',
-    #         ),
-    #     ]
