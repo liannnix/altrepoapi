liannnix/altrepoapi

commit aff6b1059828631d1b8611b478a0370272635edd

site: fix last_packages_by_branch route
- move limit clause
- use changelog fileds from mv_src_packages_last_changelog
- change packager_sub clause
- use nickname from MV

api/site/endpoints/pkgset_packages.py
@@ -644,7 +644,7 @@ class LastBranchPackages(APIWorker):
         self.packages_limit = self.args["packages_limit"]
 
         if self.packager is not None:
-            packager_sub = f"AND pkg_packager_email LIKE '{self.packager}@%'"
+            packager_sub = f"AND nickname = '{self.packager}'"
         else:
             self.packager = ""
             packager_sub = ""
@@ -702,6 +702,7 @@ class LastBranchPackages(APIWorker):
                 "pkg_release",
                 "pkg_summary",
                 "changelog_name",
+                "changelog_nickname",
                 "changelog_date",
                 "changelog_text",
                 "hash",
@@ -725,7 +726,6 @@ class LastBranchPackages(APIWorker):
 
         for pkg in packages:
             pkg["changelog_date"] = datetime_to_iso(pkg["changelog_date"])
-            pkg["changelog_nickname"] = get_nickname_from_packager(pkg["changelog_name"])
             retval.append(pkg)
 
         res = {

database/site_sql.py
@@ -1284,14 +1284,18 @@ SELECT * FROM
         pkg_version,
         pkg_release,
         pkg_summary,
-        pkg_changelog.name[1],
-        pkg_changelog.date[1],
+        CHLG.chlog_name,
+        CHLG.chlog_nick as nickname,
+        CHLG.chlog_date,
         CHLG.chlog_text
     FROM Packages
     LEFT JOIN
     (
         SELECT
             pkg_hash,
+            chlog_name,
+            chlog_nick,
+            chlog_date,
             chlog_text
         FROM mv_src_packages_last_changelog
         WHERE pkg_hash IN (
@@ -1305,7 +1309,6 @@ SELECT * FROM
             SELECT pkg_hash FROM {hsh_source}
         )
     {packager}
-    LIMIT {limit}
 ) AS RQ
 LEFT JOIN
 (
@@ -1317,6 +1320,7 @@ LEFT JOIN
     GROUP BY pkg_srcrpm_hash
 ) AS BinLastBuild ON BinLastBuild.hash = RQ.pkg_hash
 ORDER BY last_build DESC
+LIMIT {limit}
 """
 
     get_all_src_versions_from_tasks = """

commit 6a9e457d9ca773edb88b4c57c4edde84c2bf6761

site: fix bug introduced in aff6b105

api/site/endpoints/pkgset_packages.py
@@ -644,7 +644,7 @@ class LastBranchPackages(APIWorker):
         self.packages_limit = self.args["packages_limit"]
 
         if self.packager is not None:
-            packager_sub = f"AND nickname = '{self.packager}'"
+            packager_sub = f"AND pkg_packager_email LIKE '{self.packager}@%'"
         else:
             self.packager = ""
             packager_sub = ""

database/site_sql.py
@@ -1285,7 +1285,7 @@ SELECT * FROM
         pkg_release,
         pkg_summary,
         CHLG.chlog_name,
-        CHLG.chlog_nick as nickname,
+        CHLG.chlog_nick,
         CHLG.chlog_date,
         CHLG.chlog_text
     FROM Packages