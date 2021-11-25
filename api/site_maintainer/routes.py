b7856de7fe5c3ad3f54a1f6f948f6d9ce77898ae
Dmitry <fruktime@altlinux.org>
2021-11-25 15:29:15 +0300

    site: edit "by_acl" option to the repocop_by_maintainer route
    - if "by_acl" = "none", then find packages by packager (default="none")
    - if "by_acl" = "by_name", then find packages by maintainer nickname in ACL
    - if "by_acl" = "by_nick_leader", then find packages by maintainer nickname leader in ACL
    - if "by_acl" = "by_nick_or_group", then find packages by maintainer nickname and ACL groups membership
    - if "by_acl" = "by_nick_leader_and_group", then find packages by ACL groups leader in ACL

--- api/site_maintainer/routes.py
+++ api/site_maintainer/routes.py
@@ -163,10 +163,10 @@ class routeMaintainerBranches(Resource):
     },
 )
 class routeRepocopByMaintainer(Resource):
-    @ns.expect(maintainer_info_args)
+    @ns.expect(maintainer_packages_args)
     @ns.marshal_list_with(repocop_by_maintainer_model)
     def get(self):
-        args = maintainer_info_args.parse_args(strict=True)
+        args = maintainer_packages_args.parse_args(strict=True)
         url_logging(logger, g.url)
         wrk = RepocopByMaintainer(g.connection, **args)
         if not wrk.check_params():
