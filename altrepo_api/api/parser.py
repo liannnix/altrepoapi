03f9680cd7825783847f67b6161d197ad38578a5
Dmitry <fruktime@altlinux.org>
2021-12-28 17:15:16 +0300

    parser: add "\+" in the "__pkg_VR_match" regex pattern

--- altrepo_api/api/parser.py
+++ altrepo_api/api/parser.py
@@ -59,7 +59,7 @@ __known_branches = set(lut.known_branches)
 # regex patterns
 __pkg_cs_match = re.compile("^[a-fA-F0-9]+$")
 __pkg_name_match = re.compile("^[\w\.\+\-]{2,}$")  # type: ignore
-__pkg_VR_match = re.compile("^[\w\.]+$")  # type: ignore
+__pkg_VR_match = re.compile("^[\w\.\+]+$")  # type: ignore
 __pkg_groups_match = re.compile("^[A-Z][a-zA-Z0-9\+\ \/-]+$")  # type: ignore
 __pkg_disttag_match = re.compile("^[a-z0-9\+\.]+$")  # type: ignore
 __packager_name_match = re.compile("^[a-zA-Z]+[\w\.\ \-\@]*$")  # type: ignore
