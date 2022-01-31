ab9f9146482ba09fe6de6604739614b0f8e01912
Danil Shein <dshein@altlinux.org>
2022-01-31 12:15:58 +0300

    export: use wrapper function for boilerplate code

--- altrepo_api/api/export/routes.py
+++ altrepo_api/api/export/routes.py
@@ -15,9 +15,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from flask import g
-from flask_restx import Resource, abort
+from flask_restx import Resource
 
-from altrepo_api.utils import get_logger, url_logging, response_error_parser
+from altrepo_api.utils import get_logger, url_logging
+from altrepo_api.api.base import run_worker, GET_RESPONSES_400_404
 
 from .namespace import get_namespace
 from .endpoints.repology import RepologyExport
@@ -41,31 +42,17 @@ logger = get_logger(__name__)
     doc={
         "params": {"branch": "branch name"},
         "description": "Get branch info export for Repology",
-        "responses": {
-            400: "Request parameters validation error",
-            404: "Information not found in database",
-        },
+        "responses": GET_RESPONSES_400_404,
     },
 )
 class routePackageInfo(Resource):
     # @ns.expect(xxx_args)
     @ns.marshal_with(repology_export_model)
     def get(self, branch):
-        # args = xxx_args.parse_args(strict=True)
-        args = {}
         url_logging(logger, g.url)
-        wrk = RepologyExport(g.connection, branch, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = {}
+        w = RepologyExport(g.connection, branch, **args)
+        return run_worker(worker=w, args=args)
 
 
 @ns.route(
@@ -73,31 +60,17 @@ class routePackageInfo(Resource):
     doc={
         "params": {"branch": "branch name"},
         "description": "Get branch source packages for sitemap",
-        "responses": {
-            400: "Request parameters validation error",
-            404: "Information not found in database",
-        },
+        "responses": GET_RESPONSES_400_404,
     },
 )
 class routeSitemapPackages(Resource):
     # @ns.expect(xxx_args)
     @ns.marshal_with(sitemap_packages_export_model)
     def get(self, branch):
-        # args = xxx_args.parse_args(strict=True)
-        args = {}
         url_logging(logger, g.url)
-        wrk = SitemapPackages(g.connection, branch, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = {}
+        w = SitemapPackages(g.connection, branch, **args)
+        return run_worker(worker=w, args=args)
 
 
 @ns.route(
@@ -105,27 +78,14 @@ class routeSitemapPackages(Resource):
     doc={
         "params": {"branch": "branch name"},
         "description": "Get branch binary packages info",
-        "responses": {
-            400: "Request parameters validation error",
-            404: "Information not found in database",
-        },
+        "responses": GET_RESPONSES_400_404,
     },
 )
 class routePackageSetBinaries(Resource):
     @ns.expect(pkgset_packages_args)
     @ns.marshal_with(pkgset_packages_export_model)
     def get(self, branch):
-        args = pkgset_packages_args.parse_args(strict=True)
         url_logging(logger, g.url)
-        wrk = PackageSetBinaries(g.connection, branch, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = pkgset_packages_args.parse_args(strict=True)
+        w = PackageSetBinaries(g.connection, branch, **args)
+        return run_worker(worker=w, args=args)
