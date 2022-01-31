61baa5d7d075007a8c935f5d0370dbff76805005
Danil Shein <dshein@altlinux.org>
2022-01-31 15:57:14 +0300

    site_pkgset_info: use wrapper function for boilerplate code

--- altrepo_api/api/site_pkgset_info/routes.py
+++ altrepo_api/api/site_pkgset_info/routes.py
@@ -15,9 +15,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from flask import g
-from flask_restx import Resource, abort
+from flask_restx import Resource
 
-from altrepo_api.utils import get_logger, url_logging, response_error_parser
+from altrepo_api.utils import get_logger, url_logging
+from altrepo_api.api.base import run_worker, GET_RESPONSES_404, GET_RESPONSES_400_404
 
 from .namespace import get_namespace
 from .endpoints.pkgset_info import (
@@ -46,81 +47,51 @@ logger = get_logger(__name__)
     "/all_pkgsets",
     doc={
         "description": "Get package sets list",
-        "responses": {404: "Data not found in database"},
+        "responses": GET_RESPONSES_404,
     },
 )
 class routeAllPackagesets(Resource):
     # @ns.expect()
     @ns.marshal_with(all_pkgsets_model)
     def get(self):
-        args = {}
         url_logging(logger, g.url)
-        wrk = AllPackagesets(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = {}
+        w = AllPackagesets(g.connection, **args)
+        return run_worker(worker=w, args=args)
 
 
 @ns.route(
     "/all_pkgsets_with_src_count",
     doc={
         "description": ("Get package sets list with source packages count"),
-        "responses": {404: "Data not found in database"},
+        "responses": GET_RESPONSES_404,
     },
 )
 class routeAllPackagesetsSourceCount(Resource):
     # @ns.expect()
     @ns.marshal_with(all_pkgsets_model)
     def get(self):
-        args = {}
         url_logging(logger, g.url)
-        wrk = AllPackagesets(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get_with_pkgs_count()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = {}
+        w = AllPackagesets(g.connection, **args)
+        return run_worker(worker=w, args=args, run_method=w.get_with_pkgs_count)
 
 
 @ns.route(
     "/all_pkgsets_summary",
     doc={
         "description": "Get package sets list with source packages count",
-        "responses": {404: "Data not found in database"},
+        "responses": GET_RESPONSES_404,
     },
 )
 class routeAllPackagesetsSummary(Resource):
     # @ns.expect()
     @ns.marshal_with(all_pkgsets_summary_model)
     def get(self):
-        args = {}
         url_logging(logger, g.url)
-        wrk = AllPackagesets(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get_summary()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = {}
+        w = AllPackagesets(g.connection, **args)
+        return run_worker(worker=w, args=args, run_method=w.get_summary)
 
 
 @ns.route(
@@ -129,87 +100,51 @@ class routeAllPackagesetsSummary(Resource):
         "description": (
             "Get package sets list with source packages count and status info"
         ),
-        "responses": {404: "Data not found in database"},
+        "responses": GET_RESPONSES_404,
     },
 )
 class routePackagesetsSummaryStatus(Resource):
     # @ns.expect()
     @ns.marshal_with(pkgsets_summary_status_model)
     def get(self):
-        args = {}
         url_logging(logger, g.url)
-        wrk = AllPackagesets(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get_summary_status()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = {}
+        w = AllPackagesets(g.connection, **args)
+        return run_worker(worker=w, args=args, run_method=w.get_summary_status)
 
 
 @ns.route(
     "/all_pkgset_archs",
     doc={
         "description": "Get binary package archs list",
-        "responses": {
-            400: "Request parameters validation error",
-            404: "Data not found in database",
-        },
+        "responses": GET_RESPONSES_400_404,
     },
 )
 class routeAllPackagesetArchs(Resource):
     @ns.expect(all_archs_args)
     @ns.marshal_with(all_archs_model)
     def get(self):
-        args = all_archs_args.parse_args(strict=True)
         url_logging(logger, g.url)
-        wrk = AllPackagesetArchs(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = all_archs_args.parse_args(strict=True)
+        w = AllPackagesetArchs(g.connection, **args)
+        return run_worker(worker=w, args=args)
 
 
 @ns.route(
     "/all_pkgset_archs_with_src_count",
     doc={
         "description": ("Get binary package archs list with source packages count"),
-        "responses": {
-            400: "Request parameters validation error",
-            404: "Data not found in database",
-        },
+        "responses": GET_RESPONSES_400_404,
     },
 )
 class routeAllPackagesetArchsSourceCount(Resource):
     @ns.expect(all_archs_args)
     @ns.marshal_with(all_archs_model)
     def get(self):
-        args = all_archs_args.parse_args(strict=True)
         url_logging(logger, g.url)
-        wrk = AllPackagesetArchs(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get_with_src_count()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = all_archs_args.parse_args(strict=True)
+        w = AllPackagesetArchs(g.connection, **args)
+        return run_worker(worker=w, args=args, run_method=w.get_with_src_count)
 
 
 @ns.route(
@@ -218,27 +153,14 @@ class routeAllPackagesetArchsSourceCount(Resource):
         "description": (
             "Get list of package categories with count for given package set"
         ),
-        "responses": {
-            400: "Request parameters validation error",
-            404: "Package not found in database",
-        },
+        "responses": GET_RESPONSES_400_404,
     },
 )
 class routePkgsetCategoriesCount(Resource):
     @ns.expect(pkgset_categories_args)
     @ns.marshal_with(pkgset_categories_model)
     def get(self):
-        args = pkgset_categories_args.parse_args(strict=True)
         url_logging(logger, g.url)
-        wrk = PkgsetCategoriesCount(g.connection, **args)
-        if not wrk.check_params():
-            abort(
-                400,
-                message=f"Request parameters validation error",
-                args=args,
-                validation_message=wrk.validation_results,
-            )
-        result, code = wrk.get()
-        if code != 200:
-            abort(code, **response_error_parser(result))
-        return result, code
+        args = pkgset_categories_args.parse_args(strict=True)
+        w = PkgsetCategoriesCount(g.connection, **args)
+        return run_worker(worker=w, args=args)
