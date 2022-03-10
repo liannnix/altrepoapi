441487a8dbbc385def4cd974bcbf4585cf3450b0
Danil Shein <dshein@altlinux.org>
2022-03-10 12:18:12 +0300

    tests: add dependency sorting tests
    - import and modify dependency sorting unittests from 'altrepo_server' project

---
+++ tests/unit/test_deps_sorting.py
@@ -0,0 +1,66 @@
+import json
+import unittest
+
+from altrepo_api.settings import namespace as settings
+
+settings.LOG_TO_FILE = False  # type: ignore
+settings.LOG_TO_SYSLOG = False
+
+from altrepo_api.libs.dependency_sorting import SortList
+
+TEST_FILE_INPUT_STRUCT = "./tests/unit/deps_sorting_data/input_struct"
+TEST_FILE_RESULT_SORTED = "./tests/unit/deps_sorting_data/result_sorted"
+TEST_FILE_RESULTS_CIRCLE = "./tests/unit/deps_sorting_data/result_circle"
+
+
+class TestDepsSorting(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        with open(TEST_FILE_INPUT_STRUCT, "r") as fd:
+            cls.test_struct = json.loads(fd.read())
+
+    def test_sort_list(self):
+        sort = SortList(self.test_struct, "a")
+        circle, sorted_ = sort.sort_list()
+
+        with open(TEST_FILE_RESULTS_CIRCLE, "r") as fd_c, open(
+            TEST_FILE_RESULT_SORTED
+        ) as fd_s:
+            res_circle = json.loads(fd_c.read())
+            res_sorted = fd_s.read().split("\n")
+
+        assert res_circle == circle
+        assert res_sorted == sorted_
+
+    def test_numbered_list(self):
+        name_num, num_name = SortList._numbered_list(
+            ["curl", "strongswan", "SimGear", "osgEarth", "adp"]
+        )
+
+        assert {
+            "curl": 0,
+            "strongswan": 1,
+            "SimGear": 2,
+            "osgEarth": 3,
+            "adp": 4,
+        } == name_num
+        assert {
+            0: "curl",
+            1: "strongswan",
+            2: "SimGear",
+            3: "osgEarth",
+            4: "adp",
+        } == num_name
+
+    def test_search_circle_deps(self):
+        sort = SortList(None, None)
+        sort.package_reqs = self.test_struct
+
+        with open(TEST_FILE_RESULTS_CIRCLE) as fd:
+            res_circle = json.loads(fd.read())
+
+        assert res_circle == sort._search_circle_deps()
+
+
+if __name__ == "__main__":
+    unittest.main()
