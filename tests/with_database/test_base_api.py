9932d52ffccd2225d38afc205ffc87199a0b2348
Danil Shein <dshein@altlinux.org>
2022-03-10 11:18:49 +0300

    tests: fix code formatting

--- tests/with_database/test_base_api.py
+++ tests/with_database/test_base_api.py
@@ -8,12 +8,14 @@ def test_api_name(client):
     assert "name" in data
     assert data["name"] == "ALTRepo API"
 
+
 def test_api_version(client):
     response = client.get("/api/version")
     data = response.json
     assert response.status_code == 200
     assert "version" in data
     assert data["version"] == version.__version__
+
 
 def test_api_description(client):
     response = client.get("/api/version")
