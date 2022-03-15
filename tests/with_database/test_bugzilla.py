9ee27c6fd4a9b481fd1a27ac5caa55b8f71c802e
Danil Shein <dshein@altlinux.org>
2022-03-15 17:14:52 +0300

    tests: add 'bug' routes test

---
+++ tests/with_database/test_bugzilla.py
@@ -0,0 +1,67 @@
+import pytest
+from flask import url_for
+
+
+SRC_PACKAGE_IN_DB = "curl"
+BIN_PACKAGE_IN_DB = "grep"
+PACKAGE_NOT_IN_DB = "fakepackage"
+MAINTAINER_IN_DB = "rider"
+MAINTAINER_NOT_IN_DB = "fakemaintainer"
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {"package_name": SRC_PACKAGE_IN_DB, "package_type": "source", "status_code": 200},
+        {"package_name": BIN_PACKAGE_IN_DB, "package_type": "binary", "status_code": 200},
+        {"package_name": PACKAGE_NOT_IN_DB, "package_type": None, "status_code": 404},
+        {"package_name": None, "package_type": None, "status_code": 400},
+        {"package_name": SRC_PACKAGE_IN_DB, "package_type": "abc", "status_code": 400},
+    ],
+)
+def test_bugzilla_by_package(client, kwargs):
+    url = url_for("api.bug_route_bugzilla_by_package")
+    params = {}
+    for k, v in kwargs.items():
+        if k in ("status_code",):
+            continue
+        if v:
+            params[k] = v
+    response = client.get(url, query_string=params)
+    data = response.json
+    assert response.status_code == kwargs["status_code"]
+    if response.status_code == 200:
+        assert data != {}
+        assert data["length"] != 0
+        assert data["bugs"] != []
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": None, "status_code": 200},
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": "none", "status_code": 200},
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": "by_nick", "status_code": 200},
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": "by_nick_leader", "status_code": 200},
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": "by_nick_or_group", "status_code": 200},
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": "by_nick_leader_and_group", "status_code": 200},
+        {"maintainer_nickname": None, "by_acl": None, "status_code": 400},
+        {"maintainer_nickname": MAINTAINER_IN_DB, "by_acl": "abc", "status_code": 400},
+        {"maintainer_nickname": MAINTAINER_NOT_IN_DB, "by_acl": None, "status_code": 404},
+        
+    ],
+)
+def test_bugzilla_by_maintainer(client, kwargs):
+    url = url_for("api.bug_route_bugzilla_by_maintainer")
+    params = {}
+    for k, v in kwargs.items():
+        if k in ("status_code",):
+            continue
+        if v:
+            params[k] = v
+    response = client.get(url, query_string=params)
+    data = response.json
+    assert response.status_code == kwargs["status_code"]
+    if response.status_code == 200:
+        assert data != {}
+        assert data["length"] != 0
+        assert data["bugs"] != []
