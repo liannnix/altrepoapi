7714017185a2a2ef9e691022deabda0fbf5d1be3
Danil Shein <dshein@altlinux.org>
2022-04-20 14:57:17 +0300

    site_package: fix gear links for binary packages
    - use source package name in gear links for binary packages

--- altrepo_api/api/site_package/endpoints/package_info.py
+++ altrepo_api/api/site_package/endpoints/package_info.py
@@ -386,6 +386,14 @@ class PackageInfo(APIWorker):
         else:
             return lp.error
 
+        # fix gear_link for binary packages
+        if source == 0:
+            pkgname_binary = pkg_info["name"]
+            pkgname_source = list(package_archs.keys())[0]
+            in_ = f"{pkgname_binary[0]}/{pkgname_binary}.git"
+            out_ = f"{pkgname_source[0]}/{pkgname_source}.git"
+            gear_link = gear_link.replace(in_, out_)
+
         res = {
             "pkghash": str(self.pkghash),
             "request_args": self.args,
