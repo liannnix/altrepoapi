501738694f922896f1218244b1d046766f59da00
Dmitry Lyalyaev <fruktime@altlinux.org>
2023-01-26 12:30:35 +0300

    auth: auth: update /auth/logout route
    - check refresh token
    - remove user sessions in the Redis server

--- altrepo_api/api/auth/endpoints/auth_logout.py
+++ altrepo_api/api/auth/endpoints/auth_logout.py
@@ -13,9 +13,15 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import jwt
+import redis
+from flask import request
+
 from altrepo_api.api.base import APIWorker
+from altrepo_api.settings import namespace
 from .blacklisted_token import BlacklistedToken
-from ..sql import sql
+from ..constants import REFRESH_TOKEN_KEY
+from ..exceptions import ApiUnauthorized
 
 
 class AuthLogout(APIWorker):
@@ -25,17 +31,48 @@ class AuthLogout(APIWorker):
         self.conn = connection
         self.payload = payload
         self.args = kwargs
-        self.sql = sql
+        self.conn_redis = redis.from_url(namespace.REDIS_URL, db=0)
+        self.refresh_token = request.cookies.get("refresh_token")
         super().__init__()
 
     def check_params(self):
         self.logger.debug(f"args : {self.args}")
-        return True
+        self.validation_results = []
+
+        if self.refresh_token is False:
+            self.validation_results.append("User is not authorized")
+
+        if self.validation_results != []:
+            return False
+        else:
+            return True
 
     def post(self):
-        status = BlacklistedToken(self.args["token"], self.args["exp"]).post()
+        access_token = self.args["token"]
+        token_payload = jwt.decode(access_token, namespace.ADMIN_PASSWORD, algorithms=["HS256"])
+        user_sessions = self.conn_redis.hgetall(
+            REFRESH_TOKEN_KEY.format(user=token_payload.get("nickname", ""))
+        )
+        blacklisted = BlacklistedToken(access_token, self.args["exp"])
+        check_access_token = blacklisted.check_blacklist()
 
-        if not status:
-            return "Failed to log out", 400
+        if check_access_token:
+            raise ApiUnauthorized(description="Access token is not valid.")
+        else:
+            blacklisted.write_to_blacklist()
+
+        if self.refresh_token.encode() in user_sessions.keys():
+            del user_sessions[self.refresh_token.encode()]
+            if not user_sessions:
+                self.conn_redis.delete(
+                    REFRESH_TOKEN_KEY.format(user=token_payload.get("nickname", ""))
+                )
+            else:
+                self.conn_redis.hdel(
+                    REFRESH_TOKEN_KEY.format(user=token_payload.get("nickname", "")),
+                    self.refresh_token
+                )
+        else:
+            raise ApiUnauthorized(description="User not authorized")
 
         return "you successfully logged out", 201
