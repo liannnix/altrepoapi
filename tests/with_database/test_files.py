b9ca9db025af839e0c1cf2b57f2ef1454dd905a2
Dmitry Lyalyaev <fruktime@altlinux.org>
2023-03-29 15:45:18 +0300

    tests: add tests for /packages_by_file route

--- tests/with_database/test_files.py
+++ tests/with_database/test_files.py
@@ -7,6 +7,11 @@ FILE_IN_DB = [
     "Colloid-green-light/mimetypes/scalable/image-svg+xml.svg",
     "meta.cpython-37.opt-1.pyc"
 ]
+FULL_PATH_FILE_IN_DB = [
+    "/usr/share/gtksourceview-5/language-specs",
+    "/usr/share/icons/Colloid-green-light/mimetypes/scalable/image-svg+xml.svg",
+    "/usr/lib/python3/site-packages/collective/monkeypatcher/__pycache__/meta.cpython-37.opt-1.pyc"
+]
 FILE_NOT_IN_DB = "gtksourceview-5/language-specs/abc"
 INVALID_FILE_NAME = "gtksourceview-5/language-specs^"
 BRANCH_IN_DB = "sisyphus"
@@ -115,3 +120,61 @@ def test_fast_file_search(client, kwargs):
         assert data != {}
         assert data["length"] != 0
         assert data["files"] != []
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {
+            "branch": BRANCH_IN_DB,
+            "file_name": FULL_PATH_FILE_IN_DB[0],
+            "status_code": 200
+        },
+        {
+            "branch": BRANCH_IN_DB,
+            "file_name": FULL_PATH_FILE_IN_DB[1],
+            "status_code": 200
+        },
+        {
+            "branch": BRANCH_IN_DB,
+            "file_name": FULL_PATH_FILE_IN_DB[2],
+            "status_code": 200
+        },
+        {
+            "branch": BRANCH_IN_DB,
+            "file_name": FILE_IN_DB[0],
+            "status_code": 404
+        },
+        {
+            "branch": BRANCH_NOT_IN_DB,
+            "file_name": FILE_IN_DB[0],
+            "status_code": 400
+        },
+        {
+            "branch": BRANCH_IN_DB,
+            "file_name": INVALID_FILE_NAME,
+            "status_code": 400
+        },
+        {
+            "branch": BRANCH_IN_DB,
+            "file_name": FILE_NOT_IN_DB,
+            "status_code": 404
+        },
+    ],
+)
+def test_packages_by_file(client, kwargs):
+    params = {}
+    for k, v in kwargs.items():
+        if k in ("status_code",):
+            continue
+        if v is not None:
+            params[k] = v
+
+    url = url_for("api.files_route_packages_by_file")
+    response = client.get(url, query_string=params)
+    data = response.json
+    assert response.status_code == kwargs["status_code"]
+    if response.status_code == 200:
+        assert data != {}
+        assert data["length"] != 0
+        assert data["packages"] != []
