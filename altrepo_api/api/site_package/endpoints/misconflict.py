df2ce53e3c8e9f1a9e2840d5be681b4e24cd7e26
Dmitry Lyalyaev <fruktime@altlinux.org>
2023-06-07 12:33:51 +0300

    site: update /package_misconflict/<int:pkghash> route
    - group conflicting packages by architecture

--- altrepo_api/api/site_package/endpoints/misconflict.py
+++ altrepo_api/api/site_package/endpoints/misconflict.py
@@ -13,8 +13,9 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from typing import Any, NamedTuple
+from dataclasses import dataclass
+from itertools import groupby
+from typing import NamedTuple, Any
 
 from altrepo_api.api.base import APIWorker
 from altrepo_api.libs.librpm_functions import check_dependency_overlap
@@ -22,6 +23,49 @@ from altrepo_api.utils import make_tmp_table_name
 from altrepo_api.api.package.endpoints.misconflict_packages import MisconflictPackages
 
 from ..sql import sql
+
+
+class BinaryPackageArchs(NamedTuple):
+    """
+    Architecture and hash of the input binary package.
+    """
+
+    arch: str
+    pkghash: int
+
+
+@dataclass
+class BinaryPackage:
+    """
+    Information about the input binary package.
+    """
+
+    name: str
+    archs: list[BinaryPackageArchs]  # list of binary packages (arch, hash) tuples
+
+    def __init__(self, pkg_info: tuple[str, list[str, int]]):
+        self.name = pkg_info[0]
+        self.archs = [BinaryPackageArchs(*el) for el in pkg_info[1]]
+
+
+class ConflictPackage(NamedTuple):
+    """
+    Information about the conflicting package.
+    """
+
+    input_package: str
+    input_arch: str = ""
+    conflict_package: str = ""
+    version: str = ""
+    release: str = ""
+    epoch: int = 0
+    dp_name: str = ""
+    dp_version: str = ""
+    dp_flag: int = 0
+    conf_hash: int = 0
+    conf_arch: str = ""
+    files_with_conflict: list[str] = []
+    explicit: bool = False
 
 
 class PackageMisconflict(APIWorker):
@@ -55,18 +99,15 @@ class PackageMisconflict(APIWorker):
                     "args": self.args,
                 }
             )
-        binary_packages_map: dict[int, dict[str, Any]] = {}
+        binary_packages_map: dict[BinaryPackageArchs, ConflictPackage] = {}
         input_archs: dict[str, list[str]] = {}
-
-        class BinaryPackage(NamedTuple):
-            name: str
-            archs: list[tuple[str, int]]  # list of binary packages (arch, hash) tuples
-
-        for pkg in (BinaryPackage(*el[:2]) for el in response):
+        for pkg in (BinaryPackage(el[:2]) for el in response):
             input_archs[pkg.name] = []
-            for el in pkg.archs:
-                binary_packages_map[el[1]] = {"input_package": pkg.name}
-                input_archs[pkg.name].append(el[0])
+            for arch_hash in pkg.archs:
+                binary_packages_map[arch_hash] = ConflictPackage(
+                    input_package=pkg.name, input_arch=arch_hash.arch
+                )
+                input_archs[pkg.name].append(arch_hash.arch)
 
         # get information about conflicting packages
         tmp_table = make_tmp_table_name("input_pkg_hshs")
@@ -75,9 +116,9 @@ class PackageMisconflict(APIWorker):
             external_tables=[
                 {
                     "name": tmp_table,
-                    "structure": [("pkg_hash", "UInt64"),],
+                    "structure": [("pkg_hash", "UInt64"), ("arch", "String")],
                     "data": [
-                        {"pkg_hash": el}
+                        {"pkg_hash": el.pkghash, "arch": el.arch}
                         for el in binary_packages_map.keys()
                     ],
                 },
@@ -87,20 +128,9 @@ class PackageMisconflict(APIWorker):
             return self.error
 
         if response:
-            class ConflictPackage(NamedTuple):
-                conflict_package: str
-                version: str
-                release: str
-                epoch: int
-                dp_name: str
-                dp_version: str
-                dp_flag: int
-                conf_hshs: list[int]
-                archs: list[str]
-
             for el in response:
-                cp = ConflictPackage(*el[1:])
-                if el[0] in binary_packages_map.keys() and check_dependency_overlap(
+                input_hash, cp = el[0], ConflictPackage("", *el[1:])
+                if check_dependency_overlap(
                     provide_dep_name=cp.conflict_package,
                     provide_dep_version=f"{cp.version}-{cp.release}",
                     provide_dep_flags=0,
@@ -108,13 +138,17 @@ class PackageMisconflict(APIWorker):
                     require_dep_version=cp.dp_version,
                     require_dep_flags=cp.dp_flag,
                 ):
-                    binary_packages_map[el[0]].update(cp._asdict())
+                    map_key = BinaryPackageArchs(arch=cp.input_arch, pkghash=el[0])
+                    binary_packages_map[map_key] = cp._replace(
+                        input_package=binary_packages_map[map_key].input_package
+                    )
 
         conflict_hashes = set()
-        for input_hash, value in binary_packages_map.items():
-            if value.get("conf_hshs"):
-                for conf_hash in value.get("conf_hshs"):
-                    conflict_hashes.add((input_hash, conf_hash))
+        for input_arch_hash, value in binary_packages_map.items():
+            if getattr(value, "conf_hash"):
+                conflict_hashes.add(
+                    (input_arch_hash.pkghash, value.conf_hash, input_arch_hash.arch)
+                )
 
         # name for temporary table with hashes of input and conflicting package
         tmp_table = make_tmp_table_name("conflict_hashes")
@@ -124,9 +158,13 @@ class PackageMisconflict(APIWorker):
             external_tables=[
                 {
                     "name": tmp_table,
-                    "structure": [("input_hash", "UInt64"), ("conf_hash", "UInt64")],
+                    "structure": [
+                        ("input_hash", "UInt64"),
+                        ("conf_hash", "UInt64"),
+                        ("arch", "String"),
+                    ],
                     "data": [
-                        {"input_hash": hsh[0], "conf_hash": hsh[1]}
+                        {"input_hash": hsh[0], "conf_hash": hsh[1], "arch": hsh[2]}
                         for hsh in conflict_hashes
                     ],
                 },
@@ -137,13 +175,38 @@ class PackageMisconflict(APIWorker):
 
         if response:
             for el in response:
-                binary_packages_map[el[0]]["files_with_conflict"] = el[1]
-                binary_packages_map[el[0]]["explicit"] = True
+                map_key = BinaryPackageArchs(arch=el[1], pkghash=el[0])
+                binary_packages_map[map_key] = binary_packages_map[map_key]._replace(
+                    files_with_conflict=sorted(el[2]), explicit=True
+                )
 
-        resolved_conflicts = []
-        for el in binary_packages_map.values():
-            if el.get("explicit") and el not in resolved_conflicts:
-                resolved_conflicts.append(el)
+        def keyfunc(item: ConflictPackage):
+            return (
+                item.input_package,
+                item.conflict_package,
+                item.files_with_conflict,
+            )
+
+        resolved_conflicts: list[dict[str, Any]] = []
+
+        # sort conflicting packages by input package, conflicting package and files
+        conflict_packages = sorted(binary_packages_map.values(), key=keyfunc)
+
+        # group conflicting packages by input package, conflicting package and files
+        for key, group in groupby(conflict_packages, key=keyfunc):
+            group = list(group)
+
+            # convert to list of dictionaries that compatible
+            # with MisconflictPackages.find_conflicts() result
+            conflict = group[0]._asdict()
+            if conflict["explicit"]:
+                conflict["input_archs"] = set()
+                conflict["archs"] = set()
+                # group conflicting packages by architectures and list of files
+                for el in group:
+                    conflict["input_archs"].add(el.input_arch)
+                    conflict["archs"].add(el.conf_arch)
+                resolved_conflicts.append(conflict)
 
         # get misconflict
         mp = MisconflictPackages(
@@ -152,13 +215,16 @@ class PackageMisconflict(APIWorker):
             branch.lower(),
             [],
         )
-        mp.find_conflicts(tuple(binary_packages_map.keys()))
+        mp.find_conflicts(tuple([el.pkghash for el in binary_packages_map.keys()]))
 
         if mp.status:
             mp.result += resolved_conflicts
             if mp.result:
                 for el in mp.result:
-                    el["input_archs"] = input_archs.get(el["input_package"], [])
+                    # add input package architectures in the
+                    # MisconflictPackages.find_conflicts() result
+                    if not el.get("explicit"):
+                        el["input_archs"] = input_archs.get(el["input_package"], [])
                 # result processing
                 res = {
                     "request_args": self.args,
