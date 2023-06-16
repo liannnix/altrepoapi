d9f427c4bcde93c05027bea51846a5b488982715
Danil Shein <dshein@altlinux.org>
2023-06-16 11:57:11 +0300

    api/vuln: refactor code
    - fix `/vuln/bdu/packages` route result build

--- altrepo_api/api/vulnerabilities/endpoints/cve.py
+++ altrepo_api/api/vulnerabilities/endpoints/cve.py
@@ -176,6 +176,6 @@ class VulnerablePackageByCve(APIWorker):
         return {
             "request_args": self.args,
             "result": self.result_message,
-            "vuln_info": [bdu] + [vuln.asdict() for vuln in self.cve_info.values()],
+            "vuln_info": [bdu.asdict()] + [vuln.asdict() for vuln in self.cve_info.values()],
             "packages": [p.asdict() for p in self.packages_vulnerabilities],
         }, 200
