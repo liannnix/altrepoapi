fb1cdb61e2f9fddf612546c0a2380191bfd1137a
Danil Shein <dshein@altlinux.org>
2023-08-07 18:11:42 +0300

    fix code style

--- altrepo_api/api/auth/auth.py
+++ altrepo_api/api/auth/auth.py
@@ -13,6 +13,7 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import hashlib
 import ldap
 import base64
