5f2668db3d859241c3586c3bdb3d782fb7a557fb
Danil Shein <dshein@altlinux.org>
2023-09-20 18:19:37 +0300

    api/management: refactor endpoints/tools/helpers
    - add helpers to get and store errata and errata change data from DB

--- altrepo_api/api/management/endpoints/tools/helpers.py
+++ altrepo_api/api/management/endpoints/tools/helpers.py
@@ -15,11 +15,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from logging import Logger
-from typing import Any, Iterable, Protocol
+from typing import Any, Iterable, Protocol, Union
 
 from altrepo_api.api.management.sql import SQL
 
-from .base import Errata
+from .base import Errata, ErrataChange, ErrataID, Reference
 
 
 class _pAPIWorker(Protocol):
@@ -37,6 +37,47 @@ class _pAPIWorker(Protocol):
         self, request_line: Any, http_code: int = ..., **kwargs
     ) -> Any:
         ...
+
+
+def get_bulletin_by_package_update(
+    cls: _pAPIWorker, errata_id: str
+) -> Union[Errata, None]:
+    cls.status = False
+
+    response = cls.send_sql_request(
+        cls.sql.get_bulletin_by_pkg_update.format(errata_id=errata_id)
+    )
+    if not cls.sql_status:
+        return None
+
+    cls.status = True
+
+    if response:
+        r = response[0]
+        return Errata(
+            ErrataID.from_id(r[0]), *r[1:-2], [Reference(*el) for el in r[-2]], r[-1]  # type: ignore
+        )
+
+    return None
+
+
+def get_ec_id_by_package_update(
+    cls: _pAPIWorker, errata: ErrataID
+) -> Union[ErrataID, None]:
+    cls.status = False
+
+    response = cls.send_sql_request(
+        cls.sql.get_ecc_by_errata_id.format(errata_id_noversion=errata.no_version)
+    )
+    if not cls.sql_status:
+        return None
+
+    cls.status = True
+
+    if response:
+        return ErrataID.from_id(response[0][1])
+
+    return None
 
 
 def store_errata_history_records(cls: _pAPIWorker, erratas: Iterable[Errata]) -> None:
@@ -63,13 +104,39 @@ def store_errata_history_records(cls: _pAPIWorker, erratas: Iterable[Errata]) ->
 
     cls.status = False
 
-    # FIXME: mocked up
-    # _ = cls.send_sql_request(
-    #     (cls.sql.store_errata_history, errata_history_records_gen())
-    # )
-    # if not cls.sql_status:
-    #     return None
-    for r in errata_history_records_gen():
-        print("DBG", r)
+    _ = cls.send_sql_request(
+        (cls.sql.store_errata_history, errata_history_records_gen())
+    )
+    if not cls.sql_status:
+        return None
+
+    cls.status = True
+
+
+def store_errata_change_records(
+    cls: _pAPIWorker, erratas: Iterable[ErrataChange]
+) -> None:
+    def errata_change_records_gen():
+        for errata in erratas:
+            yield {
+                "ec_id": str(errata.id),
+                "ec_created": errata.created,
+                "ec_updated": errata.updated,
+                "ec_user": errata.user,
+                "ec_user_ip": errata.user_ip,
+                "ec_reason": errata.reason,
+                "ec_type": errata.type.value,
+                "ec_source": errata.source.value,
+                "ec_origin": errata.origin.value,
+                "errata_id": str(errata.errata_id),
+            }
+
+    cls.status = False
+
+    _ = cls.send_sql_request(
+        (cls.sql.store_errata_change_history, errata_change_records_gen())
+    )
+    if not cls.sql_status:
+        return None
 
     cls.status = True
