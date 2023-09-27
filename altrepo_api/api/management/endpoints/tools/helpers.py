6edd8ee7d461ca4d5016166de0a476e5fae1688c
Danil Shein <dshein@altlinux.org>
2023-09-27 17:19:00 +0300

    api/management: refactor `tools/helpers` code

--- altrepo_api/api/management/endpoints/tools/helpers.py
+++ altrepo_api/api/management/endpoints/tools/helpers.py
@@ -19,10 +19,28 @@ from logging import Logger
 from typing import Any, Iterable, NamedTuple, Protocol, Union
 
 from altrepo_api.api.management.sql import SQL
+from altrepo_api.utils import make_tmp_table_name
+from altrepo_api.api.misc import lut
 
 from .base import Errata, ErrataChange, ErrataID, ErrataManageError, Reference
-from .errata_id import ErrataIDService, check_errata_id
+from .errata import errata_hash
+from .errata_id import ErrataIDService, check_errata_id, register_branch_update_id
 from .utils import convert_dt_to_timezone_aware
+from .constants import (
+    CHECK_ERRATA_CONTENT_IS_CHANGED_FIELDS,
+    BRANCH_BULLETIN_ERRATA_SOURCE,
+    BRANCH_BULLETIN_ERRATA_TYPE,
+    ERRATA_REFERENCE_TYPE,
+    DT_NEVER,
+    BUG_REFERENCE_TYPE,
+    VULN_REFERENCE_TYPE,
+    BDU_ID_PREFIX,
+    BDU_ID_TYPE,
+    CVE_ID_PREFIX,
+    CVE_ID_TYPE,
+    MFSA_ID_PREFIX,
+    MFSA_ID_TYPE,
+)
 
 
 class _pAPIWorker(Protocol):
@@ -54,6 +72,18 @@ class _pManageErrata(_pHasErrataIDService, _pHasErrataID, Protocol):
     ...
 
 
+class Task(NamedTuple):
+    id: int
+    prev: int
+    date: datetime
+
+
+class Branch(NamedTuple):
+    name: str
+    date: datetime
+    task: int
+
+
 def _sql2errata(sql_data: tuple[Any, ...]) -> Errata:
     """Converts errata history record SQL representation to Errata object."""
 
@@ -73,6 +103,7 @@ def _sql2errata(sql_data: tuple[Any, ...]) -> Errata:
         task_state: str
         references: list[tuple[str, str]]
         hash: int
+        is_discarded: int = 0
 
     raw = ErrataRaw(*sql_data)
 
@@ -92,6 +123,15 @@ def _sql2errata(sql_data: tuple[Any, ...]) -> Errata:
         task_state=raw.task_state,
         references=[Reference(*el) for el in raw.references],
         hash=raw.hash,
+        is_discarded=bool(raw.is_discarded),
+    )
+
+
+def is_errata_equal(first: Errata, second: Errata, fields: Iterable[str]) -> bool:
+    """Checks if two errata records are equial by set of fields."""
+
+    return tuple(getattr(first, f) for f in fields) == tuple(
+        getattr(second, f) for f in fields
     )
 
 
@@ -100,8 +140,11 @@ def get_errata_contents(cls: _pManageErrata) -> None:
 
     cls.status = False
 
+    where_clause = cls.sql.get_errata_by_id_where_clause.format(
+        errata_id=cls.errata.id.id  # type: ignore
+    )
     response = cls.send_sql_request(
-        cls.sql.get_errata_info.format(errata_id=cls.errata.id)
+        cls.sql.get_errata_info_template.format(where_clause=where_clause)
     )
     if not cls.sql_status:
         return None
@@ -167,8 +210,11 @@ def check_errata_contents_is_changed(cls: _pManageErrata) -> bool:
 
     cls.status = False
 
+    where_clause = cls.sql.get_errata_by_id_where_clause.format(
+        errata_id=last_errata_id.id
+    )
     response = cls.send_sql_request(
-        cls.sql.get_errata_info.format(errata_id=last_errata_id.id)
+        cls.sql.get_errata_info_template.format(where_clause=where_clause)
     )
     if not cls.sql_status:
         return False
@@ -180,9 +226,11 @@ def check_errata_contents_is_changed(cls: _pManageErrata) -> bool:
 
     errata_from_db = _sql2errata(response[0])
     cls.status = True
-    # XXX: compare errata using only hash here due to other values except references
-    # is gathered from DB and must be consistent!
-    return cls.errata.hash != errata_from_db.hash
+    # XXX: compare errata using only hash and discard sign here due to other
+    #  values except references is gathered from DB and must be consistent!
+    return not is_errata_equal(
+        cls.errata, errata_from_db, CHECK_ERRATA_CONTENT_IS_CHANGED_FIELDS
+    )
 
 
 def check_errata_is_discarded(cls: _pManageErrata) -> bool:
@@ -223,8 +271,57 @@ def get_bulletin_by_package_update(
 
     cls.status = False
 
+    where_clause = cls.sql.get_bulletin_by_pkg_update_where_clause.format(
+        errata_id=errata_id
+    )
     response = cls.send_sql_request(
-        cls.sql.get_bulletin_by_pkg_update.format(errata_id=errata_id)
+        cls.sql.get_errata_info_template.format(where_clause=where_clause)
+    )
+    if not cls.sql_status:
+        return None
+
+    cls.status = True
+
+    if response:
+        return _sql2errata(response[0])
+
+    return None
+
+
+def get_bulletin_by_branch_date(
+    cls: _pAPIWorker, branch_state: Branch
+) -> Union[Errata, None]:
+    """Retrieves bulletin errata record from DB by branch name and date."""
+
+    cls.status = False
+
+    where_clause = cls.sql.get_bulletin_by_branch_date__where_clause.format(
+        branch=branch_state.name, date=branch_state.date
+    )
+    response = cls.send_sql_request(
+        cls.sql.get_errata_info_template.format(where_clause=where_clause)
+    )
+    if not cls.sql_status:
+        return None
+
+    cls.status = True
+
+    if response:
+        return _sql2errata(response[0])
+
+    return None
+
+
+def get_errata_by_task(cls: _pManageErrata) -> Union[Errata, None]:
+    """Retrieves package update errata from DB by task and subtask."""
+
+    cls.status = False
+
+    where_clause = cls.sql.get_errata_by_task_where_clause.format(
+        task_id=cls.errata.task_id, subtask_id=cls.errata.subtask_id
+    )
+    response = cls.send_sql_request(
+        cls.sql.get_errata_info_template.format(where_clause=where_clause)
     )
     if not cls.sql_status:
         return None
@@ -319,3 +416,232 @@ def store_errata_change_records(
         return None
 
     cls.status = True
+
+
+def find_closest_branch_state(
+    cls: _pManageErrata, task_changed: datetime
+) -> Union[Branch, None]:
+    """Finds closest branch state by given task."""
+
+    cls.status = False
+    branch = cls.errata.pkgset_name
+    task_id = cls.errata.task_id
+
+    # get DONE tasks history
+    response = cls.send_sql_request(
+        cls.sql.get_done_tasks.format(branch=branch, changed=task_changed)
+    )
+    if not cls.sql_status:
+        return None
+    if not response:
+        _ = cls.store_error(
+            {
+                "message": f"Failed to get 'DONE' tasks history for branch {branch} from {task_id}"
+            }
+        )
+
+    tasks = {t.id: t for t in (Task(*el) for el in response)}
+    if task_id not in tasks:
+        _ = cls.store_error(
+            {
+                "message": f"Task {task_id} not found in 'DONE' tasks history for {branch}"
+            }
+        )
+
+    # get nearest branch point
+    response = cls.send_sql_request(
+        cls.sql.get_nearest_branch_point.format(branch=branch, changed=task_changed)
+    )
+    if not cls.sql_status:
+        return None
+    if not response:
+        _ = cls.store_error(
+            {"message": f"Failed to find nearest state of {branch} from {task_id}"}
+        )
+        return None
+
+    branch_state = Branch(*response[0])
+    if branch_state.task not in tasks:
+        _ = cls.store_error(
+            {"message": "Branch state is inconsistent with task history"},
+            severity=cls.LL.ERROR,  # type: ignore
+        )
+        return None
+
+    cls.status = True
+    return branch_state
+
+
+def build_new_bulletin(cls: _pManageErrata, branch_state: Branch) -> Errata:
+    """Builds new bulletin errata record object from errata
+    and registers new errata ID for it."""
+
+    eid = register_branch_update_id(cls.eid_service, branch_state.date.year)
+    bulletin = Errata(
+        id=(ErrataID.from_id(eid.id)),
+        type=BRANCH_BULLETIN_ERRATA_TYPE,
+        source=BRANCH_BULLETIN_ERRATA_SOURCE,
+        created=branch_state.date,
+        updated=eid.updated,
+        pkg_hash=0,
+        pkg_name="",
+        pkg_version="",
+        pkg_release="",
+        pkgset_name=branch_state.name,
+        task_id=0,
+        subtask_id=0,
+        task_state="",
+        references=[Reference(type=ERRATA_REFERENCE_TYPE, link=cls.errata.id.id)],  # type: ignore
+        hash=0,
+    )
+    return bulletin.update(hash=errata_hash(bulletin))
+
+
+class Vulnerability(NamedTuple):
+    id: str
+    type: str
+    summary: str = ""
+    score: float = 0.0
+    severity: str = ""
+    url: str = ""
+    modified_date: datetime = DT_NEVER
+    published_date: datetime = DT_NEVER
+    references: list[str] = list()
+    is_valid: bool = False
+
+
+class Bug(NamedTuple):
+    id: int
+    summary: str = ""
+    last_changed: datetime = DT_NEVER
+    is_valid: bool = False
+
+
+def bug2vuln(bug: Bug) -> Vulnerability:
+    return Vulnerability(
+        id=str(bug.id),
+        type=str(BUG_REFERENCE_TYPE).upper(),
+        summary=bug.summary,
+        url=f"#{bug.id}",
+        modified_date=bug.last_changed,
+        published_date=bug.last_changed,
+        is_valid=bug.is_valid,
+    )
+
+
+def empty_vuln(vuln_id: str) -> Vulnerability:
+    vuln_type = ""
+    if vuln_id.startswith(CVE_ID_PREFIX):
+        vuln_type = CVE_ID_TYPE
+        normalized_id = vuln_id.lower()
+        vuln_url = f"{lut.nvd_cve_base}/{normalized_id}"
+    elif vuln_id.startswith(BDU_ID_PREFIX):
+        vuln_type = BDU_ID_TYPE
+        normalized_id = vuln_id.removeprefix(BDU_ID_PREFIX)
+        vuln_url = f"{lut.fstec_bdu_base}/{normalized_id}"
+    elif vuln_id.startswith(MFSA_ID_PREFIX):
+        vuln_type = MFSA_ID_TYPE
+        normalized_id = vuln_id.replace("MFSA ", "mfsa").replace("MFSA-", "mfsa")
+        vuln_url = f"{lut.mfsa_base}/{normalized_id}"
+    else:
+        vuln_url = f"#{vuln_id}"
+    return Vulnerability(id=vuln_id, type=vuln_type, url=vuln_url)
+
+
+def get_vulns_by_ids(
+    cls: _pAPIWorker, vuln_ids: Iterable[str]
+) -> Union[list[Vulnerability], None]:
+    cls.status = False
+
+    tmp_table = make_tmp_table_name("vuln_ids")
+
+    response = cls.send_sql_request(
+        cls.sql.get_vulns_by_ids.format(tmp_table=tmp_table),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("vuln_id", "String")],
+                "data": [{"vuln_id": vuln_id} for vuln_id in vuln_ids],
+            },
+        ],
+    )
+    if not cls.sql_status:
+        return None
+    if response is None:
+        _ = cls.store_error({"message": "No vulnerabilities data found in DB"})
+        return None
+
+    cls.status = True
+    return [Vulnerability(*row, is_valid=True) for row in response]
+
+
+def get_bugs_by_ids(cls: _pAPIWorker, bug_ids: Iterable[int]) -> Union[list[Bug], None]:
+    cls.status = False
+
+    tmp_table = make_tmp_table_name("bz_ids")
+
+    response = cls.send_sql_request(
+        cls.sql.get_bugs_by_ids.format(tmp_table=tmp_table),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("bz_id", "UInt32")],
+                "data": [{"bz_id": bz_id} for bz_id in bug_ids],
+            },
+        ],
+    )
+    if not cls.sql_status:
+        return None
+    if response is None:
+        _ = cls.store_error({"message": "No bugs data found in DB"})
+        return None
+
+    cls.status = True
+    return [Bug(*row, is_valid=True) for row in response]
+
+
+def collect_errata_vulnerabilities_info(cls: _pManageErrata):
+    vulns: dict[str, Vulnerability] = {}
+    bugs: dict[int, Bug] = {}
+
+    cls.status = False
+
+    # collect bugs and vulnerabilities from errata
+    vulns.update(
+        {
+            v.id: v
+            for v in (
+                empty_vuln(ref.link)
+                for ref in cls.errata.references
+                if ref.type == VULN_REFERENCE_TYPE
+            )
+        }
+    )
+    bugs.update(
+        {
+            b.id: b
+            for b in (
+                Bug(id=int(ref.link))
+                for ref in cls.errata.references
+                if ref.type == BUG_REFERENCE_TYPE
+            )
+        }
+    )
+
+    # get vulnerabilities info
+    vulns_data = get_vulns_by_ids(cls, (vuln_id for vuln_id in vulns))
+    if not cls.status or vulns_data is None:
+        return None
+
+    vulns.update({vuln.id: vuln for vuln in vulns_data})
+
+    # get bugs info
+    bugs_data = get_bugs_by_ids(cls, (bug_id for bug_id in bugs))
+    if not cls.status or bugs_data is None:
+        return None
+
+    bugs.update({bug.id: bug for bug in bugs_data})
+    vulns.update({str(b_id): bug2vuln(bug) for b_id, bug in bugs.items()})
+
+    cls.status = True
+    return [vuln for vuln in vulns.values()]
