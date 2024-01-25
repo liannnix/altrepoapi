944a6abb8ffed1f82559eb8e4e78a3ff67aa89a5
Danil Shein <dshein@altlinux.org>
2024-01-25 13:10:52 +0300

    api/management: refactor `processing/errata_builder`

--- altrepo_api/api/management/endpoints/processing/errata_builder.py
+++ altrepo_api/api/management/endpoints/processing/errata_builder.py
@@ -19,12 +19,9 @@ from typing import Iterable
 from altrepodb_libs import PackageCveMatch, VersionCompareResult
 
 from altrepo_api.api.base import APIWorker
-from altrepo_api.utils import make_tmp_table_name
 
 from .base import (
     ErrataBuilderError,
-    CpeMatchVersions,
-    CveVersionsMatch,
     CveCpmHashes,
     ErrataPoint,
     PackageTask,
@@ -36,14 +33,18 @@ from .base import (
     package_is_vulnerable,
     version_release_compare,
 )
-from .sql import sql
-from ..tools.base import Errata, ErrataID, Reference
-from ..tools.changelog import (
-    ChangelogRecord,
-    PackageChangelog,
-    split_evr,
-    vulns_from_package_changelog,
+from .helpers import (
+    get_pkgs_changelog,
+    get_pkgs_done_tasks,
+    get_pkgs_versions,
+    get_build_tasks_by_pkg_nevr,
+    get_cves_versions_matches,
+    # get_related_erratas_by_cve_ids,
+    get_related_erratas_by_pkgs_names,
 )
+from .sql import sql
+from ..tools.base import Errata
+from ..tools.changelog import vulns_from_package_changelog
 
 
 class ErrataBuilder(APIWorker):
@@ -55,270 +56,24 @@ class ErrataBuilder(APIWorker):
         self.sql = sql
         super().__init__()
 
-    def _get_related_erratas_by_pkgs_names(
-        self, packages: Iterable[str], exclude_discarded: bool
-    ) -> dict[str, Errata]:
-        self.status = False
-        erratas = {}
-
-        # collect erratas using package' names
-        tmp_table = make_tmp_table_name("pkg_names")
-
-        response = self.send_sql_request(
-            self.sql.get_erratas_by_pkgs_names.format(
-                branches=self.branches, tmp_table=tmp_table
-            ),
-            external_tables=[
-                {
-                    "name": tmp_table,
-                    "structure": [("pkg_name", "String")],
-                    "data": [{"pkg_name": n} for n in packages],
-                },
-            ],
-        )
-        if not self.sql_status:
-            return {}
-        for el in response:
-            errata = Errata(
-                id=ErrataID.from_id(el[0]),
-                created=el[1],
-                updated=el[2],
-                hash=el[3],
-                type=el[4],
-                source=el[5],
-                references=[Reference(t, l) for t, l in zip(el[6], el[7])],
-                pkg_hash=el[8],
-                pkg_name=el[9],
-                pkg_version=el[10],
-                pkg_release=el[11],
-                pkgset_name=el[12],
-                task_id=el[13],
-                subtask_id=el[14],
-                task_state=el[15],
-                is_discarded=bool(el[17]),
-            )
-            # XXX: skip discarded erratas here
-            if exclude_discarded and errata.is_discarded:
-                continue
-            erratas[errata.id.id] = errata  # type: ignore
-
-        self.status = True
-        return erratas
-
-    def _get_related_erratas_by_cve_ids(
-        self, cve_ids: Iterable[str]
-    ) -> dict[str, Errata]:
-        self.status = False
-        erratas = {}
-
-        tmp_table = make_tmp_table_name("cve_ids")
-
-        response = self.send_sql_request(
-            self.sql.get_erratas_by_cve_ids.format(
-                branches=self.branches, tmp_table=tmp_table
-            ),
-            external_tables=[
-                {
-                    "name": tmp_table,
-                    "structure": [("cve_id", "String")],
-                    "data": [{"cve_id": c} for c in cve_ids],
-                },
-            ],
-        )
-        if not self.sql_status:
-            return {}
-        for el in response:
-            errata = Errata(
-                id=ErrataID.from_id(el[0]),
-                created=el[1],
-                updated=el[2],
-                hash=el[3],
-                type=el[4],
-                source=el[5],
-                references=[Reference(t, l) for t, l in zip(el[6], el[7])],
-                pkg_hash=el[8],
-                pkg_name=el[9],
-                pkg_version=el[10],
-                pkg_release=el[11],
-                pkgset_name=el[12],
-                task_id=el[13],
-                subtask_id=el[14],
-                task_state=el[15],
-                is_discarded=bool(el[17]),
-            )
-            erratas[errata.id.id] = errata  # type: ignore
-
-        self.status = True
-        return erratas
-
-    def _get_pkgs_versions(
-        self, pkgs_hashes: Iterable[int]
-    ) -> dict[int, PackageVersion]:
-        self.status = False
-
-        tmp_table = make_tmp_table_name("pkgs_hashes")
-
-        response = self.send_sql_request(
-            self.sql.get_packages_versions.format(tmp_table=tmp_table),
-            external_tables=[
-                {
-                    "name": tmp_table,
-                    "structure": [("pkg_hash", "UInt64")],
-                    "data": [{"pkg_hash": h} for h in pkgs_hashes],
-                },
-            ],
-        )
-        if not self.sql_status or not response:
-            return {}
-
-        self.status = True
-        return {p.hash: p for p in (PackageVersion(*el) for el in response)}
-
-    def _get_pkgs_changelog(
-        self, pkgs_hashes: Iterable[int]
-    ) -> dict[int, PackageChangelog]:
-        res: dict[int, PackageChangelog] = {}
-        self.status = False
-
-        tmp_table = make_tmp_table_name("pkgs_hashes")
-
-        response = self.send_sql_request(
-            self.sql.get_packages_changelogs.format(tmp_table=tmp_table),
-            external_tables=[
-                {
-                    "name": tmp_table,
-                    "structure": [("pkg_hash", "UInt64")],
-                    "data": [{"pkg_hash": h} for h in pkgs_hashes],
-                },
-            ],
-        )
-        if not self.sql_status or not response:
-            return {}
-
-        for el in response:
-            if el[0] not in res:
-                res[el[0]] = PackageChangelog(el[0], list())
-            res[el[0]].changelog.append(ChangelogRecord(*el[1:]))
-
-        self.status = True
-        return res
-
-    def _get_build_tasks_by_pkg_nevr(
-        self, name: str, evr: str
-    ) -> dict[str, list[PackageTask]]:
-        """Returns dict[branch, list[PackageTask]] for given package' name and evr."""
-
-        self.status = False
-        res: dict[str, list[PackageTask]] = {}
-
-        epoch, version, release = split_evr(evr)
-
-        where_clause = self.sql.get_done_tasks_by_nevr_cluse.format(
-            branches=self.branches,
-            name=name,
-            epoch=epoch,
-            version=version,
-            release=release,
-        )
-
-        response = self.send_sql_request(
-            self.sql.get_done_tasks.format(where_clause=where_clause)
-        )
-        if not self.sql_status:
-            return res
-
-        for p in (PackageTask(*el) for el in response):
-            res.setdefault(p.branch, []).append(p)
-
-        self.status = True
-        return res
-
-    def _get_pkgs_done_tasks(
-        self, pkgs_names: Iterable[str]
-    ) -> dict[str, list[PackageTask]]:
-        """Returns dict[name, list[PackageTask]] for given packages names."""
-
-        self.status = False
-        res: dict[str, list[PackageTask]] = {}
-
-        tmp_table = make_tmp_table_name("pkgs_names")
-
-        where_clause = self.sql.get_done_tasks_by_packages_clause.format(
-            branches=self.branches, tmp_table=tmp_table
-        )
-
-        response = self.send_sql_request(
-            self.sql.get_done_tasks.format(where_clause=where_clause),
-            external_tables=[
-                {
-                    "name": tmp_table,
-                    "structure": [("pkg_name", "String")],
-                    "data": [{"pkg_name": n} for n in pkgs_names],
-                },
-            ],
-        )
-        if not self.sql_status or not response:
-            return res
-
-        for p in (PackageTask(*el) for el in response):
-            res.setdefault(p.name, []).append(p)
-
-        self.status = True
-        return res
-
-    def _get_cves_versions_matches(
-        self, cve_cpm_hashes: Iterable[CveCpmHashes]
-    ) -> dict[int, list[CveVersionsMatch]]:
-        self.status = False
-        res: dict[int, list[CveVersionsMatch]] = {}
-
-        tmp_table = make_tmp_table_name("pkgs_hashes")
-
-        response = self.send_sql_request(
-            self.sql.get_cve_versions_matches.format(tmp_table=tmp_table),
-            external_tables=[
-                {
-                    "name": tmp_table,
-                    "structure": [
-                        ("vuln_hash", "UInt64"),
-                        ("cpe_hash", "UInt64"),
-                        ("cpm_version_hash", "UInt64"),
-                    ],
-                    "data": [e._asdict() for e in cve_cpm_hashes],
-                },
-            ],
-        )
-        if not self.sql_status or not response:
-            return res
-
-        for el in response:
-            cvm = CveVersionsMatch(
-                id=el[0],
-                hashes=CveCpmHashes(*el[1:4]),
-                versions=CpeMatchVersions(*el[4:]),
-            )
-            res.setdefault(cvm.hashes.vuln_hash, []).append(cvm)
-
-        self.status = True
-        return res
-
     def _get_possible_errata_points(
         self,
         pkgs_cve_matches: Iterable[PackageCveMatch],
         pkgs_versions: dict[int, PackageVersion],
     ) -> set[ErrataPoint]:
         # get packages history
-        pkgs_tasks = self._get_pkgs_done_tasks({m.pkg_name for m in pkgs_cve_matches})
+        pkgs_tasks = get_pkgs_done_tasks(self, {m.pkg_name for m in pkgs_cve_matches})
         if not self.status:
             self.store_error({"message": "Failed to get packages tasks from DB"})
             raise ErrataBuilderError("Failed to get packages tasks from DB")
 
         # get CVE versions matches
-        cve_cpm_versions = self._get_cves_versions_matches(
+        cve_cpm_versions = get_cves_versions_matches(
+            self,
             {
                 CveCpmHashes(m.vuln_hash, m.cpm_cpe_hash, m.cpm_version_hash)
                 for m in pkgs_cve_matches
-            }
+            },
         )
         if not self.status:
             raise ErrataBuilderError("Failed to get CVE versions matches from DB")
@@ -372,15 +127,15 @@ class ErrataBuilder(APIWorker):
         pkgs_hashes = {m.pkg_hash for m in pkgs_cve_matches}
 
         # get packages versions
-        pkgs_versions = self._get_pkgs_versions(pkgs_hashes)
+        pkgs_versions = get_pkgs_versions(self, pkgs_hashes)
         print("DBG", pkgs_hashes)
         if not self.status:
             self.store_error({"message": "Failed to get packages versions from DB"})
             raise ErrataBuilderError("Failed to get packages versions from DB")
 
         # get existing erratas by packages names
-        all_erratas = self._get_related_erratas_by_pkgs_names(
-            packages=pkgs_names, exclude_discarded=True
+        all_erratas = get_related_erratas_by_pkgs_names(
+            self, packages=pkgs_names, exclude_discarded=True
         )
         if not self.status:
             self.store_error({"message": "Failed to get erratas by package' names"})
@@ -407,8 +162,8 @@ class ErrataBuilder(APIWorker):
             return [], []
 
         # get packages changelogs
-        pkgs_changelogs = self._get_pkgs_changelog(
-            {ep.task.hash for ep in possible_errata_points}
+        pkgs_changelogs = get_pkgs_changelog(
+            self, {ep.task.hash for ep in possible_errata_points}
         )
         if not self.status:
             self.store_error({"message": "Failed to get packages changelogs from DB"})
@@ -472,7 +227,7 @@ class ErrataBuilder(APIWorker):
                     # found exact changelog record that closes given CVE
                     task = get_closest_task(
                         ep.task.branch,
-                        self._get_build_tasks_by_pkg_nevr(ep.task.name, chlog.evr),
+                        get_build_tasks_by_pkg_nevr(self, ep.task.name, chlog.evr),
                     )
                     if task:
                         _ep = ErrataPoint(task=task, prev_task=None, cvm=ep.cvm)
--- altrepo_api/api/management/endpoints/processing/helpers.py
+++ altrepo_api/api/management/endpoints/processing/helpers.py
@@ -15,9 +15,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from logging import Logger
-from typing import Any, Protocol
+from typing import Any, Iterable, Protocol
 
+from altrepo_api.utils import make_tmp_table_name
+
+from .base import (
+    CpeMatchVersions,
+    CveCpmHashes,
+    CveVersionsMatch,
+    PackageTask,
+    PackageVersion,
+)
 from .sql import SQL
+from ..tools.base import Errata, ErrataID, Reference
+from ..tools.changelog import ChangelogRecord, PackageChangelog, split_evr
 
 
 class _pAPIWorker(Protocol):
@@ -35,3 +46,261 @@ class _pAPIWorker(Protocol):
         self, request_line: Any, http_code: int = ..., **kwargs
     ) -> Any:
         ...
+
+
+class _pHasBranches(_pAPIWorker, Protocol):
+    branches: tuple[str, ...]
+
+
+def get_related_erratas_by_pkgs_names(
+    cls: _pHasBranches, packages: Iterable[str], exclude_discarded: bool
+) -> dict[str, Errata]:
+    cls.status = False
+    erratas = {}
+
+    # collect erratas using package' names
+    tmp_table = make_tmp_table_name("pkg_names")
+
+    response = cls.send_sql_request(
+        cls.sql.get_erratas_by_pkgs_names.format(
+            branches=cls.branches, tmp_table=tmp_table
+        ),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("pkg_name", "String")],
+                "data": [{"pkg_name": n} for n in packages],
+            },
+        ],
+    )
+    if not cls.sql_status:
+        return {}
+    for el in response:
+        errata = Errata(
+            id=ErrataID.from_id(el[0]),
+            created=el[1],
+            updated=el[2],
+            hash=el[3],
+            type=el[4],
+            source=el[5],
+            references=[Reference(t, l) for t, l in zip(el[6], el[7])],
+            pkg_hash=el[8],
+            pkg_name=el[9],
+            pkg_version=el[10],
+            pkg_release=el[11],
+            pkgset_name=el[12],
+            task_id=el[13],
+            subtask_id=el[14],
+            task_state=el[15],
+            is_discarded=bool(el[17]),
+        )
+        # XXX: skip discarded erratas here
+        if exclude_discarded and errata.is_discarded:
+            continue
+        erratas[errata.id.id] = errata  # type: ignore
+
+    cls.status = True
+    return erratas
+
+
+def get_related_erratas_by_cve_ids(
+    cls: _pHasBranches, cve_ids: Iterable[str]
+) -> dict[str, Errata]:
+    cls.status = False
+    erratas = {}
+
+    tmp_table = make_tmp_table_name("cve_ids")
+
+    response = cls.send_sql_request(
+        cls.sql.get_erratas_by_cve_ids.format(
+            branches=cls.branches, tmp_table=tmp_table
+        ),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("cve_id", "String")],
+                "data": [{"cve_id": c} for c in cve_ids],
+            },
+        ],
+    )
+    if not cls.sql_status:
+        return {}
+    for el in response:
+        errata = Errata(
+            id=ErrataID.from_id(el[0]),
+            created=el[1],
+            updated=el[2],
+            hash=el[3],
+            type=el[4],
+            source=el[5],
+            references=[Reference(t, l) for t, l in zip(el[6], el[7])],
+            pkg_hash=el[8],
+            pkg_name=el[9],
+            pkg_version=el[10],
+            pkg_release=el[11],
+            pkgset_name=el[12],
+            task_id=el[13],
+            subtask_id=el[14],
+            task_state=el[15],
+            is_discarded=bool(el[17]),
+        )
+        erratas[errata.id.id] = errata  # type: ignore
+
+    cls.status = True
+    return erratas
+
+
+def get_pkgs_versions(
+    cls: _pAPIWorker, pkgs_hashes: Iterable[int]
+) -> dict[int, PackageVersion]:
+    cls.status = False
+
+    tmp_table = make_tmp_table_name("pkgs_hashes")
+
+    response = cls.send_sql_request(
+        cls.sql.get_packages_versions.format(tmp_table=tmp_table),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("pkg_hash", "UInt64")],
+                "data": [{"pkg_hash": h} for h in pkgs_hashes],
+            },
+        ],
+    )
+    if not cls.sql_status or not response:
+        return {}
+
+    cls.status = True
+    return {p.hash: p for p in (PackageVersion(*el) for el in response)}
+
+
+def get_pkgs_changelog(
+    cls: _pAPIWorker, pkgs_hashes: Iterable[int]
+) -> dict[int, PackageChangelog]:
+    res: dict[int, PackageChangelog] = {}
+    cls.status = False
+
+    tmp_table = make_tmp_table_name("pkgs_hashes")
+
+    response = cls.send_sql_request(
+        cls.sql.get_packages_changelogs.format(tmp_table=tmp_table),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("pkg_hash", "UInt64")],
+                "data": [{"pkg_hash": h} for h in pkgs_hashes],
+            },
+        ],
+    )
+    if not cls.sql_status or not response:
+        return {}
+
+    for el in response:
+        if el[0] not in res:
+            res[el[0]] = PackageChangelog(el[0], list())
+        res[el[0]].changelog.append(ChangelogRecord(*el[1:]))
+
+    cls.status = True
+    return res
+
+
+def get_build_tasks_by_pkg_nevr(
+    cls: _pHasBranches, name: str, evr: str
+) -> dict[str, list[PackageTask]]:
+    """Returns dict[branch, list[PackageTask]] for given package' name and evr."""
+
+    cls.status = False
+    res: dict[str, list[PackageTask]] = {}
+
+    epoch, version, release = split_evr(evr)
+
+    where_clause = cls.sql.get_done_tasks_by_nevr_cluse.format(
+        branches=cls.branches,
+        name=name,
+        epoch=epoch,
+        version=version,
+        release=release,
+    )
+
+    response = cls.send_sql_request(
+        cls.sql.get_done_tasks.format(where_clause=where_clause)
+    )
+    if not cls.sql_status:
+        return res
+
+    for p in (PackageTask(*el) for el in response):
+        res.setdefault(p.branch, []).append(p)
+
+    cls.status = True
+    return res
+
+
+def get_pkgs_done_tasks(
+    cls: _pHasBranches, pkgs_names: Iterable[str]
+) -> dict[str, list[PackageTask]]:
+    """Returns dict[name, list[PackageTask]] for given packages names."""
+
+    cls.status = False
+    res: dict[str, list[PackageTask]] = {}
+
+    tmp_table = make_tmp_table_name("pkgs_names")
+
+    where_clause = cls.sql.get_done_tasks_by_packages_clause.format(
+        branches=cls.branches, tmp_table=tmp_table
+    )
+
+    response = cls.send_sql_request(
+        cls.sql.get_done_tasks.format(where_clause=where_clause),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [("pkg_name", "String")],
+                "data": [{"pkg_name": n} for n in pkgs_names],
+            },
+        ],
+    )
+    if not cls.sql_status or not response:
+        return res
+
+    for p in (PackageTask(*el) for el in response):
+        res.setdefault(p.name, []).append(p)
+
+    cls.status = True
+    return res
+
+
+def get_cves_versions_matches(
+    cls: _pAPIWorker, cve_cpm_hashes: Iterable[CveCpmHashes]
+) -> dict[int, list[CveVersionsMatch]]:
+    cls.status = False
+    res: dict[int, list[CveVersionsMatch]] = {}
+
+    tmp_table = make_tmp_table_name("pkgs_hashes")
+
+    response = cls.send_sql_request(
+        cls.sql.get_cve_versions_matches.format(tmp_table=tmp_table),
+        external_tables=[
+            {
+                "name": tmp_table,
+                "structure": [
+                    ("vuln_hash", "UInt64"),
+                    ("cpe_hash", "UInt64"),
+                    ("cpm_version_hash", "UInt64"),
+                ],
+                "data": [e._asdict() for e in cve_cpm_hashes],
+            },
+        ],
+    )
+    if not cls.sql_status or not response:
+        return res
+
+    for el in response:
+        cvm = CveVersionsMatch(
+            id=el[0],
+            hashes=CveCpmHashes(*el[1:4]),
+            versions=CpeMatchVersions(*el[4:]),
+        )
+        res.setdefault(cvm.hashes.vuln_hash, []).append(cvm)
+
+    cls.status = True
+    return res
