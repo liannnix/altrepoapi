b9ae82f9dd6fde07246c733a881f0cf1a11e0498
Danil Shein <dshein@altlinux.org>
2024-02-07 18:52:16 +0300

    api/management: refactor code
    - ChangeReason: add `clone` method

--- altrepo_api/api/management/endpoints/tools/base.py
+++ altrepo_api/api/management/endpoints/tools/base.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import copy
 import json
 
 from datetime import datetime
@@ -216,6 +217,13 @@ class ChangeReason(NamedTuple):
     actor: UserInfo
     message: str
     details: dict[str, Any]
+
+    def clone(self) -> "ChangeReason":
+        return ChangeReason(
+            actor=self.actor,
+            message=self.message,
+            details=copy.deepcopy(self.details)
+        )
 
     def serialize(self) -> str:
         res = self._asdict()
