liannnix/altrepoapi

commit 6d39015244ed50bc87659ef1f1058d8ff99c28b6

api/auth: refactor code
- auth: fix error handling
- user_roles: fix user data handling

altrepo_api/api/auth/token/user_roles.py
@@ -25,18 +25,16 @@ from .token import STORAGE
 
 
 def store_errata_user(
-    conn: ConnectionProtocol, user: str, group: str, roles: list[str]
+    conn: ConnectionProtocol, user_data: dict[str, Any]
 ) -> tuple[bool, str]:
     # get original user name from DB using aliases table
-    conn.request_line = sql.get_original_user_name.format(user=user)
+    conn.request_line = sql.get_original_user_name.format(user=user_data["user"])
     sql_status, response = conn.send_request()
     if not sql_status or not response:
         return False, f"Database error info: {response}"
     user = response[0][0]
     # store user data
-    conn.request_line = sql.store_errata_user.format(
-        user=user, group=group, roles=roles
-    )
+    conn.request_line = sql.store_errata_user.format(**user_data)
     sql_status, response = conn.send_request()
     if not sql_status:
         return False, f"Database error info: {response}"
@@ -73,7 +71,7 @@ class UserRolesCache:
         )
 
         # store user data to the database
-        status, message = store_errata_user(self.conn, user, group, roles)
+        status, message = store_errata_user(self.conn, user_data)
         if not status:
             self.logger.error("Failed to store user data: %s", message)
 

altrepo_api/api/auth/auth.py
@@ -113,12 +113,12 @@ def check_auth_ldap(
         entries = ldap_client.search_ext_s(user_dn, ldap.SCOPE_SUBTREE) or []  # type: ignore
         for uid, props in entries:  # type: ignore
             if f"uid={user_dn}" == uid:
-                if names := props["displayName"]:
-                    try:
+                try:
+                    if names := props["displayName"]:
                         return names[0].decode()
-                    except UnicodeDecodeError:
-                        # do not fail if we can't determine user display name
-                        pass
+                except (KeyError, TypeError, UnicodeDecodeError):
+                    # do not fail if we can't determine user display name
+                    pass
 
     def is_member_of_ldap_group(group: str) -> bool:
         user_dn = namespace.LDAP_USER_SEARCH % {"user": user}

commit f04a4bbcbe0171cb3b1ee2e050905c883d8056a7

api/auth/token: fix user nae conversion error introduced in 6d390152

altrepo_api/api/auth/token/user_roles.py
@@ -32,7 +32,7 @@ def store_errata_user(
     sql_status, response = conn.send_request()
     if not sql_status or not response:
         return False, f"Database error info: {response}"
-    user = response[0][0]
+    user_data["user"] = response[0][0]
     # store user data
     conn.request_line = sql.store_errata_user.format(**user_data)
     sql_status, response = conn.send_request()